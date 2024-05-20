8c7fccd9a87a5a9322fa0d34762cb3dc4d441397
Ianny <iannyraquel252488@gmail.com>
2024-05-20 01:19:08 -0300

    Criando os temas da página

--- src/JavaScript/script.js
+++ src/JavaScript/script.js
@@ -1,3 +1,4 @@
+// SLIDESHOW
 const images = ['./src/Imagens/Banner/banner.jpg', './src/Imagens/Banner/banner2.jpeg', './src/Imagens/Banner/banner3.jpg', './src/Imagens/Banner/banner4.jpeg'];
 let currentIndex = 0;
 
@@ -12,3 +13,12 @@ changeSlide();
 
 // Define um intervalo para chamar a função de mudar o slide a cada 5 segundos (5000 milissegundos)
 setInterval(changeSlide, 5000);
+
+// TROCAR A COR DO BACKGROUND
+function trocar(corDeFundo) {
+    // Trocar a cor de fundo do body
+    document.body.style.backgroundColor = corDeFundo;
+
+    const textoSobreImagem = document.querySelector('.texto-sobre-imagem');
+    textoSobreImagem.style.background = `linear-gradient(to top, ${corDeFundo}, rgba(0, 0, 0, 0.342))`;
+}
